c6e9ec0c11e167ac22c662598652eab7e0255516
fgriese <f.griese@uke.de>
2017-06-16 15:35:15 +0200

    first running script

--- test/CommunicationTest.jl
+++ test/CommunicationTest.jl
@@ -1,6 +1,8 @@
 using SerialPorts
 
 serialports = list_serialports()
-s = SerialPort("/dev/ttyACM1", 250000)
-write(s, "Hello World!\n")
-close(s)
+s = SerialPort("/dev/ttyS0", 19200)
+#baurate 19200, 8 Datenbit, 1 Stoppbit, keine Parität
+r1=write(s, "@07\r")
+r2=write(s, "@0R7\r")
+#close(s)
