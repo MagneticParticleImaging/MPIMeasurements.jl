2807b109ad1dfd19b97ae8b4b28bebae456cf8e4
Tobias Knopp <tobias@knoppweb.de>
2017-07-13 09:13:00 +0200

    small changes

--- src/TransferFunction/TransferFunction.jl
+++ src/TransferFunction/TransferFunction.jl
@@ -38,6 +38,12 @@ function load_tf(filename::String)
   return TransferFunction(data[2:end,1],data[2:end,2],data[2:end,3])
 end
 
+function save_tf(tf::TransferFunction, filename::String)
+  writecsv(filename,hcat( vcat("frequency",tf.freq),
+                          vcat("amplitude", abs(tf.data)),
+                          vcat("phase", angle(tf.data)) ))
+end
+
 function load_tf_fromMatthias(filenameAmp::String, filenamePh::String)
   data = readcsv(filenameAmp)
   ph = readcsv(filenamePh)
@@ -50,7 +56,9 @@ function load_tf_fromMatthias(filenameAmp::String, filenamePh::String)
   return TransferFunction(freq, tf.data)
 end
 
-function correct_Trafo(tf::TransferFunction,trafo::TransferFunction)
+# The following is only required if a transformater was used to convert the
+# differential signal into a regular signal
+function correct_Transformator(tf::TransferFunction,trafo::TransferFunction)
   tf.data[:]./=trafo.data[:]
   tf.data[:] ./= (tf.freq.*2*pi*im)
   tf.data[1] = 1
@@ -100,29 +108,23 @@ function _convertTFFuncs()
   save_tf(t1M,"tfdata/Trafo1M.csv")
 
   a = load_tf_fromMatthias("measurements/XMAG.CSV", "measurements/XPH.CSV")
-  b =  correct_Trafo(a,t1M)
+  b =  correct_Transformator(a,t1M)
   save_tf(b,"tfdata/PreinstalledXMG.csv")
 
   a = load_tf_fromMatthias("measurements/YMAG.CSV", "measurements/YPH.CSV")
-  b =  correct_Trafo(a,t1M)
+  b =  correct_Transformator(a,t1M)
   save_tf(b,"tfdata/PreinstalledYMG.csv")
 
   a = load_tf_fromMatthias("measurements/ZMAG.CSV", "measurements/ZPH.CSV")
-  b =  correct_Trafo(a,t1M)
+  b =  correct_Transformator(a,t1M)
   save_tf(b,"tfdata/PreinstalledZMG.csv")
 
   a = load_tf_fromMatthias("measurements/GMAG.CSV", "measurements/GGPPH.CSV")
-  b =  correct_Trafo(a,t1M)
+  b =  correct_Transformator(a,t1M)
   save_tf(b,"tfdata/Gradio1.csv")
 
 end
 
-
-function save_tf(tf::TransferFunction, filename::String)
-  writecsv(filename,hcat( vcat("frequency",tf.freq),
-                          vcat("amplitude", abs(tf.data)),
-                          vcat("phase", angle(tf.data)) ))
-end
 
 function tf_receive_chain(id::String)
 
@@ -153,7 +155,7 @@ end
 
 
 function list_tf()
-  list = readdir(Pkg.dir("MPILib","src","TransferFunction","tfdata"))
+  list = readdir(Pkg.dir("MPIMeasurements","src","TransferFunction","tfdata"))
   stripped_list = [ splitext(a)[1] for a in list ]
   return stripped_list
 end
@@ -171,79 +173,3 @@ function plot_tf(tf::TransferFunction; fignum=312)
   plot(freq./1000, angle(tf[freq]),lw=2,"b")
 
 end
-
-
-
-
-# Example
-#b = BrukerFile("/opt/mpidata/20150909_155300_LCurveStudy_1_1/1")
-#rxchain = tf_receive_chain(b)
-#
-#semilogy(frequencies(b),squeeze(abs(rxchain[:,3])))
-#
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-####
-# Some 1D MPI data
-####
-
-#datadir = "/opt/mpidata/20141218_165058_1DSensitivity_1_1/"
-#
-#bSF = BrukerFile(datadir*"57")
-#bMeas = BrukerFile(datadir*"61")
-#
-# regular system matrix based reconstruction
-#c = reconstruction(bSF, bMeas, lambd=0.1, minFreq=80e3, iterations=3, frames=1)
-#
-#figure(1)
-#plot(c)
-#
-## Determine transfer function
-#
-#(S, SMeas, transferFunction) = simulate1DSF(bSF, D=25e-9)
-#
-## xspace reco
-
-#u = getMeasurementsFT(bMeas)
-#
-#correctForFFPSpeed(v)
-#gridDataOnSpatialIntervall(v)
-
-
-
-
-#=
-function tf_receive_chain(f::BrukerFile, path::AbstractString=Pkg.dir("MPILib","src","TransferFunction/"))
-  pathx = path*"HH_RXCHAIN_X_20151006.S2P"
-  pathy = path*"HH_RXCHAIN_Y_20151006.S2P"
-  pathz = path*"HH_RXCHAIN_Z_20151006.S2P"
-  rxfreq = 10000.0:931.8323952470294:1500000.0
-
-  ax = load_tf_fromUlrich(pathx)
-  ay = load_tf_fromUlrich(pathy)
-  az = load_tf_fromUlrich(pathz)
-
-  freq = frequencies(f)
-  rxchain = Complex64[]
-  append!(rxchain, ax[freq])
-  append!(rxchain, ay[freq])
-  append!(rxchain, az[freq])
-  rxchain = reshape(rxchain, length(freq),3)
-  return rxchain
-end
-=#
