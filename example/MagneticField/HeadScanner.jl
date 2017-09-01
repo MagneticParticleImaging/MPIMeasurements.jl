9f20c99e31738843a3b37a955410152e121e951d
Tobias Knopp <tobias@knoppweb.de>
2017-09-01 21:51:00 +0200

    add head scanner example

--- example/MagneticField/HeadScanner.jl
+++ example/MagneticField/HeadScanner.jl
@@ -8,38 +8,44 @@ using HDF5
 shp = [3,3,3]
 fov = [3.0,3.0,3.0]u"mm"
 ctr = [0,0,0]u"mm"
-caG = CartesianGridPositions(shp,fov,ctr)
+positions = CartesianGridPositions(shp,fov,ctr)
 
-# create Scanner
-#bR = IselRobot("/dev/ttyUSB1")
-bR = DummyRobot()
+robot = DummyRobot()
+scannerSetup = hallSensorRegularScanner
 
-#mfMeasObj = MagneticFieldMeas(GaussMeter("/dev/ttyUSB2"),u"T",Vector{Vector{typeof(1.0u"m")}}(),Vector{Vector{typeof(1.0u"T")}}())
-
-struct DummyMeasObj <: MeasObj
+struct MyMeasObj <: MeasObj
+  rp::RedPitaya
 end
+
+rp = RedPitaya("192.168.1.20")
+measObj = MyMeasObj(rp)
 
 # Initialize GaussMeter with standard settings
 #setStandardSettings(mfMeasObj.gaussMeter)
 
 # define preMoveAction
-function preMA(measObj::DummyMeasObj, pos::Vector{typeof(1.0u"mm")})
+function preMA(measObj::MyMeasObj, pos::Vector{typeof(1.0u"mm")})
   println("moving to next position...")
 
 end
 
 # define postMoveAction
-function postMA(measObj::DummyMeasObj, pos::Vector{typeof(1.0u"mm")})
+function postMA(measObj::MyMeasObj, pos::Vector{typeof(1.0u"mm")})
   println("post action: ", pos)
+
+  newvoltage = rand()
+  value(rp,"AOUT0",newvoltage)
+  println( "Set DC source $newvoltage   $(value(measObj.rp,"AIN2")) " )
+
   #sleep(1.0)
   #getPosition(measObj, pos)
   #getXYZValues(measObj)
   #println(measObj.magneticField[end])
 end
 
-res = performTour!(bR, caG, DummyMeasObj(), preMA, postMA)
+res = performTour!(robot, scannerSetup, positions, measObj, preMA, postMA)
 
 #move back to park position after measurement has finished
-movePark(bR)
+movePark(robot)
 
 #saveMagneticFieldAsHDF5(mfMeasObj, "/home/nmrsu/measurmenttmp/2_5Tm.hd5", 2.5u"Tm^-1")
