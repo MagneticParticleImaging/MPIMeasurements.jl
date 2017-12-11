3c12385bbd585f6cc60fce328819274e347ca2ea
Tobias Knopp <tobias@knoppweb.de>
2017-12-11 16:10:20 +0100

    minor updates

---
+++ example/DAQ/OnlineMeasurement.jl
@@ -0,0 +1,19 @@
+using MPIMeasurements
+
+scanner = MPIScanner("HeadScanner.toml")
+daq = getDAQ(scanner)
+
+params = toDict(daq.params)
+
+params["studyName"]="TestTobi"
+params["studyDescription"]="A very cool measurement"
+params["scannerOperator"]="Tobi"
+params["dfStrength"]=[10e-3]
+#params["acqNumAverages"]=1
+
+x = linspace(0,1,4)
+params["acqFFValues"] = repeat( cat(1,x,reverse(x[2:end-1])),inner=4)
+params["acqNumPeriods"]=length(params["acqFFValues"])
+
+
+measurementCont(daq, params, controlPhase=true)
--- example/MPS/OnlineMeasurement.jl
+++ example/MPS/OnlineMeasurement.jl
@@ -4,10 +4,11 @@ scanner = MPIScanner("MPS.toml")
 daq = getDAQ(scanner)
 
 params = toDict(daq.params)
+
 params["studyName"]="TestTobi"
 params["studyDescription"]="A very cool measurement"
 params["scannerOperator"]="Tobi"
 params["dfStrength"]=[20e-3]
-params["acqNumAverages"]=100
+params["acqNumAverages"]=1000
 
-measurementCont(daq, controlPhase=true)
+measurementCont(daq, params, controlPhase=true)
