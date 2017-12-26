fefc5fe0e755137b22cfb9fca38f983cc8f21d03
Tobias Knopp <tobias@knoppweb.de>
2017-12-26 10:47:12 +0100

    check in WIP files

--- example/DAQ/OnlineMeasurement.jl
+++ example/DAQ/OnlineMeasurement.jl
@@ -9,11 +9,15 @@ params["studyName"]="TestTobi"
 params["studyDescription"]="A very cool measurement"
 params["scannerOperator"]="Tobi"
 params["dfStrength"]=[10e-3]
+params["decimation"]=32
+params["acqNumFrames"]=5000
 #params["acqNumAverages"]=1
 
-x = linspace(0,1,10)
+x = linspace(0,1,5)
+#params["acqFFValues"] = [0.0]
 params["acqFFValues"] = repeat( cat(1,x,reverse(x[2:end-1])),inner=5)
 params["acqNumPeriodsPerFrame"]=length(params["acqFFValues"])
 
 
-measurementCont(daq, params, controlPhase=true)
+#measurementCont(daq, params, controlPhase=true, showFT=false)
+u = measurement(daq, params, controlPhase=false)
