b11964fc82ced33f7e4d2713ab6768aaf759e491
Tobias Knopp <tobias@knoppweb.de>
2018-04-18 12:05:36 +0200

    minor changes

--- example/DAQ/OnlineMeasurement.jl
+++ example/DAQ/OnlineMeasurement.jl
@@ -8,13 +8,13 @@ params = toDict(daq.params)
 params["studyName"]="TestTobi"
 params["studyDescription"]="A very cool measurement"
 params["scannerOperator"]="Tobi"
-params["dfStrength"]=[10e-3]
-params["acqNumAverages"]=1
-
+params["dfStrength"]=[5e-3]  #[5e-3]
+params["acqNumAverages"]=1000
+#params["calibFieldToVolt"]=[1]
 x = linspace(0,1,3)
-#params["acqFFValues"] = [0.0]
-params["acqFFValues"] = repeat( cat(1,x,reverse(x[2:end-1])),inner=5)
+params["acqFFValues"] = [0.0]
+#params["acqFFValues"] = repeat( cat(1,x,reverse(x[2:end-1])),inner=5)
 params["acqNumPeriodsPerFrame"]=length(params["acqFFValues"])
 
 
-measurementCont(daq, params, controlPhase=true, showFT=false)
+measurementCont(daq, params, controlPhase=true, showFT=true)
