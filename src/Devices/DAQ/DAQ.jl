213636798ef3db05d0863f41aaea11ed2de07643
nHackel <nHackel@users.noreply.github.com>
2021-09-08 08:53:36 +0200

    Updated DAQ interface and DummyDAQ

--- src/Devices/DAQ/DAQ.jl
+++ src/Devices/DAQ/DAQ.jl
@@ -145,14 +145,25 @@ end
 #include("Plotting.jl")
 #include("Parameters.jl")
 
-@mustimplement setup(daq::AbstractDAQ, sequence::Sequence)
 @mustimplement startTx(daq::AbstractDAQ)
 @mustimplement stopTx(daq::AbstractDAQ)
-@mustimplement setTxParams(daq::AbstractDAQ, correctionAmp, correctionPhase; convoluted=true)
-#@mustimplement trigger(daq::AbstractDAQ)
+@mustimplement setTxParams(daq::AbstractDAQ, sequence::Sequence)
 @mustimplement currentFrame(daq::AbstractDAQ)
-@mustimplement readData(daq::AbstractDAQ, startFrame, numFrames, numBlockAverages)
+@mustimplement readData(daq::AbstractDAQ, startFrame, numFrames)
 @mustimplement readDataPeriods(daq::AbstractDAQ, startPeriod, numPeriods)
+@mustimplement refToField(daq::AbstractDAQ, d::Int64)
+
+@mustimplement setSequenceParams(daq::AbstractDAQ, sequence::Sequence) # Needs to be able to update seqeuence parameters
+@mustimplement prepareSequence(daq::AbstractDAQ, sequence::Sequence) # Sequence can be prepared before started
+@mustimplement endSequence(daq::AbstractDAQ) # Sequence can be ended outside of producer
+@mustimplement prepareTx(daq::AbstractDAQ, sequence::Sequence; allowControlLoop = true) # Tx can be set outside of producer
+# Producer prepares a proper sequence if allowed too, then starts it and writes the resulting chunks to the channel
+@mustimplement asyncProducer(channel::Channel, daq::AbstractDAQ, numFrames; prepTx = true, prepSeq = true, endSeq = true) 
+@mustimplement channelType(daq::AbstractDAQ) # What is written to the channel
+@mustimplement AsyncBuffer(daq::AbstractDAQ) # Buffer structure that contains channel elements
+@mustimplement updateAsyncBuffer!(buffer::AsyncBuffer, chunk) # Adds channel element to buffer
+@mustimplement retrieveMeasAndRef!(buffer::AsyncBuffer, daq::AbstractDAQ) # Retrieve all available measurement and reference frames from the buffer
+
 @mustimplement numTxChannelsTotal(daq::AbstractDAQ)
 @mustimplement numRxChannelsTotal(daq::AbstractDAQ)
 @mustimplement numTxChannelsActive(daq::AbstractDAQ)
@@ -195,3 +206,19 @@ calibration(daq::AbstractDAQ, channelID::AbstractString) = channel(daq, channelI
 include("RedPitayaDAQ.jl")
 include("DummyDAQ.jl")
 include("SimpleSimulatedDAQ.jl")
+
+function asyncProducer(channel::Channel, daq::AbstractDAQ, numFrames; prepTx = true, prepSeq = true, endSeq = true)
+  if prepTx
+      prepareTx(daq)
+  end
+  if prepSeq
+      setSequenceParams(daq)
+      prepareSequence(daq)
+  end
+  
+  endFrame = startProducer(channel, daq, numFrames)
+
+  if endSeq
+      endSequence(daq, endFrame)
+  end
+end
--- src/Devices/DAQ/DummyDAQ.jl
+++ src/Devices/DAQ/DummyDAQ.jl
@@ -1,8 +1,9 @@
 export DummyDAQ, DummyDAQParams
 
-Base.@kwdef struct DummyDAQParams <: DeviceParams
+Base.@kwdef mutable struct DummyDAQParams <: DeviceParams
   samplesPerPeriod::Int
-  sendFrequency::typeof(1u"kHz")
+  amplitude::Float32 = 1.0
+  frequency::Float32 = 1.0
 end
 DummyDAQParams(dict::Dict) = params_from_dict(DummyDAQParams, dict)
 
@@ -20,18 +21,21 @@ function init(daq::DummyDAQ)
 end
 
 checkDependencies(daq::DummyDAQ) = true
-
-function setup(daq::DummyDAQ, sequence::Sequence)
-  
-end
-
 function startTx(daq::DummyDAQ)
 end
 
 function stopTx(daq::DummyDAQ)
 end
 
-function setTxParams(daq::DummyDAQ, amplitude, phase; postpone=false)
+function setTxParams(daq::DummyDAQ, amplitude, frequency)
+  daq.params.amplitude = amplitude
+  daq.params.frequency = frequency
+end
+function setTxParams(daq::DummyDAQ, sequence::Sequence)
+  temp = electricalTxChannels(sequence)
+  channels = [channel for channel in temp if channel isa periodicElectricalTxChannels]
+  component = components(channels[1])
+  setTxParams(daq, amplitude(component), divider(component))
 end
 
 function currentFrame(daq::DummyDAQ)
@@ -40,6 +44,129 @@ end
 
 function currentPeriod(daq::DummyDAQ)
     return 1;
+end
+
+function disconnect(daq::DummyDAQ)
+end
+
+enableSequence(daq::DummyDAQ, enable::Bool, numFrames=0,
+              ffRampUpTime=0.4, ffRampUpFraction=0.8) = 1
+
+function readData(daq::DummyDAQ, startFrame, numFrames)
+    uMeas = zeros(daq.params.samplesPerPeriod,1,1,1)
+    uRef = zeros(daq.params.samplesPerPeriod,1,1,1)
+
+    uMeas[:,1,1,1] = sin.(range(0,2*pi, length=daq.params.samplesPerPeriod))
+    uRef[:,1,1,1] = sin.(range(0,2*pi, length=daq.params.samplesPerPeriod))
+
+    return uMeas, uRef
+end
+
+function readDataPeriods(daq::DummyDAQ, startPeriod, numPeriods)
+    uMeas = zeros(daq.params.samplesPerPeriod,1,1,1)
+    uRef = zeros(daq.params.samplesPerPeriod,1,1,1)
+
+    uMeas[:,1,1,1] = sin.(range(0,2*pi, length=daq.params.samplesPerPeriod))
+    uRef[:,1,1,1] = sin.(range(0,2*pi, length=daq.params.samplesPerPeriod))
+    
+    return uMeas, uRef
+end
+refToField(daq::DummyDAQ, d::Int64) = 0.0
+
+mutable struct DummyAsyncBuffer <: AsyncBuffer
+    samples::Union{Matrix{Float32}, Nothing}
+end
+
+function channelType(daq::DummyDAQ)
+    return Matrix{Float32}
+end
+
+function AsyncBuffer(daq::DummyDAQ)
+    return DummyAsyncBuffer(nothing)
+end
+
+function frameAverageBufferSize(daq::DummyDAQ, frameAverages) 
+    # 1 Rx channel and 1 period per frame
+    return daq.params.rxNumSamplingPoints, 1, 1, frameAverages
+end
+
+function updateAsyncBuffer!(buffer::DummyAsyncBuffer, chunk)
+    samples = chunk
+    if !isnothing(buffer.samples)
+        buffer.samples = hcat(buffer.samples, samples)
+    else
+        buffer.samples = samples
+    end
+end
+
+function retrieveMeasAndRef!(buffer::DummyAsyncBuffer, daq::DummyDAQ)
+    unusedSamples = buffer.samples
+    samples = unusedSamples
+    frames = nothing
+    samplesInBuffer = size(samples)[2]
+    framesInBuffer = div(samplesInBuffer, daq.params.samplesPerPeriod)
+    
+    if framesInBuffer > 0
+        samplesToConvert = samples[:, 1:(daq.params.samplesPerPeriod * framesInBuffer)]
+        temp = reshape(samplesToConvert, 2, daq.params.samplesPerPeriod, 1, 1)
+        frames = zeros(Float32, daq.params.samplesPerPeriod, 2, 1, 1)
+        frames[:, 1, :, :] = temp[1, :, :, :]
+        frames[:, 2, :, :] = temp[2, :, :, :]
+        
+        if (daq.params.samplesPerPeriod * framesInBuffer) + 1 <= samplesInBuffer
+            unusedSamples = samples[:, (daq.params.samplesPerPeriod * framesInBuffer) + 1:samplesInBuffer]
+        else 
+          unusedSamples = nothing
+        end
+  
+    end
+
+    buffer.samples = unusedSamples
+    
+    uMeas = nothing
+    uRef = nothing
+    if !isnothing(frames)
+        uMeas = frames[:, [1], :, :]
+        uRef = frames[:, [2], :, :]
+    end
+
+    return uMeas, uRef
+end
+
+function startProducer(channel::Channel, daq::DummyDAQ)
+    startTx(daq)    
+    startFrame = 1
+    endFrame = numFrames + 1
+    currentFrame = startFrame
+    try 
+        while currentFrame < endFrame
+            samples = zeros(Float32, 2, daq.params.samplesPerPeriod)
+            samples[1, :] = daq.params.amplitude * sin.(daq.params.frequency .* range(0,2*pi, length=daq.params.samplesPerPeriod))
+            samples[2, :] = daq.params.amplitude * sin.(daq.params.frequency .* range(0,2*pi, length=daq.params.samplesPerPeriod))
+            put!(channel, samples)
+            currentFrame += 1
+        end
+    catch e
+        @error e
+    end
+    return endFrame
+end
+
+function prepareTx(daq::DummyDAQ, sequence::Sequence; allowControlLoop = true)
+    # NOP
+end
+
+function setSequenceParams(daq::DummyDAQ, sequence::Sequence)
+    # NOP
+end
+
+function prepareSequence(daq::DummyDAQ, sequence::Sequence)
+    # NOP
+end
+
+
+function endSequence(daq::DummyDAQ, endFrame)
+    # NOP
 end
 
 function readData(daq::DummyDAQ, startFrame, numFrames, numBlockAverages)
