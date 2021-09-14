644fd127311da25584a9bac2439da0ce52197600
Jonas Schumacher <jonas.beuke@gmx.de>
2021-09-14 09:31:03 +0200

    Add LakeShore F71 gaussmeter device

--- src/Devices/GaussMeter/LakeShoreF71.jl
+++ src/Devices/GaussMeter/LakeShoreF71.jl
@@ -1,19 +1,36 @@
 using Sockets
-using PyCall
+using SCPIInstruments
 
-export LakeShoreF71GaussMeter, LakeShoreF71GaussMeterParams, getXValue, getYValue, getZValue
+export LakeShoreF71GaussMeter, LakeShoreF71GaussMeterParams, LakeShoreF71GaussMeterConnectionModes,
+       F71_CM_TCP, F71_CM_USB, F71_MM_AC, F71_MM_DC, F71_MM_HIFR
 
-# @enum LakeShoreF71GaussMeterModes
+@enum LakeShoreF71GaussMeterConnectionModes begin
+  F71_CM_TCP
+  F71_CM_USB
+end
 
-# end
+@enum LakeShoreF71GaussMeterMeasurementModes begin
+  F71_MM_DC
+  F71_MM_AC
+  F71_MM_HIFR
+end
 
 Base.@kwdef struct LakeShoreF71GaussMeterParams <: DeviceParams
+  connectionMode::LakeShoreF71GaussMeterConnectionModes = F71_CM_USB
   ip::IPAddr = ip"192.168.2.2"
+  port::Integer = 7777
+  comport::String = "COM4"
+  baudrate::Integer = 115200
+
+  measurementMode::LakeShoreF71GaussMeterMeasurementModes = F71_MM_DC
+
   coordinateTransformation::Matrix{Float64} = Matrix{Float64}(I,(3,3))
-  #mode::
 end
 LakeShoreF71GaussMeterParams(dict::Dict) = params_from_dict(LakeShoreF71GaussMeterParams, dict)
 
+"""
+ 
+"""
 Base.@kwdef mutable struct LakeShoreF71GaussMeter <: GaussMeter
   "Unique device ID for this device as defined in the configuration."
   deviceID::String
@@ -22,23 +39,62 @@ Base.@kwdef mutable struct LakeShoreF71GaussMeter <: GaussMeter
   "Vector of dependencies for this device."
   dependencies::Dict{String, Union{Device, Missing}}
 
-  driver = missing
+  driver::Union{SCPIInstrument, Missing} = missing
 end
 
 function init(gauss::LakeShoreF71GaussMeter)
-  @info "Initializing simulated gaussmeter unit with ID `$(gauss.deviceID)`."
-  fromLakeshore = PyCall.pyimport("lakeshore")
-  gauss.driver = fromLakeshore.Teslameter(ip_address=ipaddress(gauss))
+  @info "Initializing LakShore F71 gaussmeter unit with ID `$(gauss.deviceID)`."
+  if gauss.params.connectionMode == F71_CM_TCP
+    gauss.driver = TCPSCPIInstrument(gauss.params.ip, gauss.params.port)
+  elseif gauss.params.connectionMode == F71_CM_USB
+    gauss.driver = SerialSCPIInstrument(gauss.params.comport, gauss.params.baudrate, flow_control=true)
+  end
+
+  setMeasurementMode(gauss, gauss.params.measurementMode)
+
+  # Always use Tesla
+  command(gauss.driver, "UNIT:FIELD TESLA")
 end
+
+Base.close(gauss::LakeShoreF71GaussMeter) = close(gauss.driver)
 
 ipaddress(gauss::LakeShoreF71GaussMeter) = gauss.params.ip
 
 checkDependencies(gauss::LakeShoreF71GaussMeter) = true
 
+function setMeasurementMode(gauss::LakeShoreF71GaussMeter, mode::LakeShoreF71GaussMeterMeasurementModes)
+  if mode == F71_MM_DC
+    SCPIInstruments.command(gauss.driver, "SENS:MODE DC")
+  elseif mode == F71_MM_AC
+    SCPIInstruments.command(gauss.driver, "SENS:MODE AC")
+  elseif mode == F71_MM_HIFR
+    SCPIInstruments.command(gauss.driver, "SENS:MODE HIFR")
+  else
+    error("Unsupported measurement mode `$mode`.")
+  end
+end
+
 getXValue(gauss::LakeShoreF71GaussMeter) = getXYZValues(gauss)[1]
 getYValue(gauss::LakeShoreF71GaussMeter) = getXYZValues(gauss)[2]
 getZValue(gauss::LakeShoreF71GaussMeter) = getXYZValues(gauss)[3]
-getXYZValues(gauss::GaussMeter) = gauss.driver.get_rms_field_xyz()u"T"
-getTemperature(gauss::LakeShoreF71GaussMeter) = gauss.driver.get_temperature()u"°C"
-getFrequency(gauss::LakeShoreF71GaussMeter) = gauss.driver.get_frequency()u"Hz"
+
+function getXYZValues(gauss::LakeShoreF71GaussMeter)
+  if gauss.params.measurementMode == F71_MM_DC
+    values = parse.(Float64, split(SCPIInstruments.query(gauss.driver, "FETCH:DC? ALL"), ","))[2:4]u"T"
+  else
+    values = parse.(Float64, split(SCPIInstruments.query(gauss.driver, "FETCH:RMS? ALL"), ","))[2:4]u"T"
+  end
+  return gauss.params.coordinateTransformation*values
+end
+
+getTemperature(gauss::LakeShoreF71GaussMeter) = parse(Float64, SCPIInstruments.query(gauss.driver, "FETCH:TEMP?"))u"°C"
+
+function getFrequency(gauss::LakeShoreF71GaussMeter)
+  if gauss.params.measurementMode == F71_MM_DC
+    return 0.0u"Hz"
+  else
+    return parse(Float64, SCPIInstruments.query(gauss.driver, "FETCH:FREQ?"))u"Hz"
+  end
+end
+
 calculateFieldError(gauss::LakeShoreF71GaussMeter, magneticField::Vector{<:Unitful.BField}) = 0.0u"mT"
--- test/Devices/GaussMeter/LakeShoreF71Test.jl
+++ test/Devices/GaussMeter/LakeShoreF71Test.jl
@@ -1,13 +1,33 @@
 using Gtk
 using Sockets
-params = LakeShoreF71GaussMeterParams(ip=ip"192.168.2.2")
+params = LakeShoreF71GaussMeterParams(ip=ip"192.168.2.2", connectionMode=TCP)
 gauss = LakeShoreF71GaussMeter(deviceID="lakeshoref71", params=params, dependencies=Dict{String, Union{Device, Missing}}())
 
 if ask_dialog("You are trying to start the LakeShore F71 hardware test. Please ensure that the device is turned on and working.","Cancel","Start test")
-    # Werte abfragen und dann vergleichen
-    
-    
-    @test ask_dialog("Did everything seem normal?")
+	setMeasurementMode(gauss, F71_MM_DC)
+	@test ask_dialog("Is the measurement mode set to DC?", "No", "Yes")
+
+	setMeasurementMode(gauss, F71_MM_AC)
+	@test ask_dialog("Is the measurement mode set to AC?", "No", "Yes")
+
+	setMeasurementMode(gauss, F71_MM_HIFR)
+	@test ask_dialog("Is the measurement mode set to HIFR?", "No", "Yes")
+
+	info_dialog("Please position a magnet near the probe.")
+
+	currentValue = parse(input_dialog("Please enter the current reading of the x-channel.", "")[2])
+	@test isapprox(currentValue, ustrip(u"T", getXValue(gauss)), rtol=0.1e-3)
+
+	currentValue = parse(input_dialog("Please enter the current reading of the y-channel.", "")[2])
+	@test isapprox(currentValue, ustrip(u"T", getYValue(gauss)), rtol=0.1e-3)
+
+	currentValue = parse(input_dialog("Please enter the current reading of the z-channel.", "")[2])
+	@test isapprox(currentValue, ustrip(u"T", getZValue(gauss)), rtol=0.1e-3)
+
+	@test ask_dialog("Is the probe's temperature reading of $(getTemperature(gauss)) °C a probable value?")
+	@test ask_dialog("Is the probe's frequency reading of $(getFrequency(gauss)) Hz a probable value?")
+	
+	@test ask_dialog("Did everything seem normal?")
 else
-    @test_broken 0
+  @test_broken 0
 end
