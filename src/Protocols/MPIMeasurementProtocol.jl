ddff6f1fa378fe4b5a30ec1afa5c0a92836e7832
nHackel <nHackel@users.noreply.github.com>
2021-10-07 16:56:50 +0200

    Init version of updated measurement protocol WIP

--- src/Protocols/MPIMeasurementProtocol.jl
+++ src/Protocols/MPIMeasurementProtocol.jl
@@ -1,11 +1,14 @@
 export MPIMeasurementProtocol, MPIMeasurementProtocolParams
 
 Base.@kwdef struct MPIMeasurementProtocolParams <: ProtocolParams
-  testParam::Float64
-  testParam2::Int64
-  testParam3::AbstractString
-  testParam4::Bool
-  #sequenceName::AbstractString
+  "Sequence to measure"
+  sequence::Union{Sequence, Nothing} = nothing
+  "Foreground frames to measure. Overwrites sequence frames"
+  fgFrames::Int64 = 1
+  "Background frames to measure. Overwrites sequence frames"
+  bgFrames::Int64 = 1
+  "If unset no background measurement will be taken"
+  measureBackground::Bool = true
 end
 MPIMeasurementProtocolParams(dict::Dict) = params_from_dict(MPIMeasurementProtocolParams, dict)
 
@@ -15,73 +18,73 @@ Base.@kwdef mutable struct MPIMeasurementProtocol <: Protocol
   scanner::MPIScanner
   params::MPIMeasurementProtocolParams
   biChannel::BidirectionalChannel{ProtocolEvent}
+  executeTask::Union{Task, Nothing} = nothing
+
+  bgMeas::Array{Float32, 4} = zeros(Float32,0,0,0,0)
   done::Bool = false
   cancelled::Bool = false
   finishAcknowledged::Bool = false
+  restart::Bool = false
 end
 
-function init(protocol::MPIMeasurementProtocol)
-  return BidirectionalChannel{ProtocolEvent}(protocol.biChannel)
+function _init(protocol::MPIMeasurementProtocol)
+  if isnothing(protocol.params.sequence)
+    throw(IllegalStateException("Protocol requires a sequence"))
+  end
+  protocol.done = false
+  protocol.cancelled = false
+  protocol.finishAcknowledged = false
+  protocol.bgMeas = zeros(Float32,0,0,0,0)
 end
 
-function execute(protocol::MPIMeasurementProtocol)
+function _execute(protocol::MPIMeasurementProtocol)
   @info "Measurement protocol started"
 
-  handleEvents(protocol)
-  if protocol.cancelled
-    close(protocol.biChannel)
-    return
-  end
-  
-  if !isnothing(protocol.scanner.currentSequence)
-    measurement(protocol)
-    protocol.done = true
-  end
 
-  handleEvents(protocol)
-  if protocol.cancelled
-    close(protocol.biChannel)
-    return
-  end
+  while true
+    protocol.restart = false
+    performMeasurement(protocol)
 
-
-  put!(protocol.biChannel, FinishedNotificationEvent())
-  while !protocol.finishAcknowledged
-    handleEvents(protocol)
-    if protocol.cancelled
-      close(protocol.biChannel)
-      return
+    put!(protocol.biChannel, FinishedNotificationEvent())
+    while !(protocol.finishAcknowledged || protocol.restart)
+      handleEvents(protocol) 
+      protocol.cancelled && throw(CancelException())
+      sleep(0.05)  
     end
-    sleep(0.01)
+    !protocol.finishAcknowledged || break
+    protocol.restart && put!(protocol.biChannel, OperationSuccessfulEvent(RestartEvent()))
   end
 
   @info "Protocol finished."
   close(protocol.biChannel)
 end
 
-function measurement(protocol::MPIMeasurementProtocol)
-  scanner = protocol.scanner
-  measState = asyncMeasurement(scanner)
-  producer = measState.producer
-  consumer = measState.consumer
-  result = nothing
-
-  try 
-    Base.wait(producer)
-  catch e 
-    if !isa(e, TaskFailedException) 
-      @error "Unexpected error"
-      @error e
+function performMeasurement(protocol::MPIMeasurementProtocol)
+  if length(protocol.bgMeas) == 0 && protocol.params.measureBackground
+    message = "No background measurement was taken\nShould one be measured now?"
+    if askConfirmation(protocol, message)
+      acqNumFrames(protocol.params.sequence, protocol.params.bgFrames)
+      measurement(protocol)
+      protocol.bgMeas = protocol.scanner.seqMeasState.buffer
     end
   end
 
-  try
-    Base.wait(producer)
-  catch e
-    if !isa(e, TaskFailedException)
-      @error "Unexpected error"
-      @error e
-    end
+  acqNumFrames(protocol.params.sequence, protocol.params.fgFrames)
+  measurement(protocol)
+end
+
+function measurement(protocol::MPIMeasurementProtocol)
+  # Start async measurement
+  scanner = protocol.scanner
+  measState = asyncMeasurement(scanner, protocol.params.sequence)
+  producer = measState.producer
+  consumer = measState.consumer
+  
+  # Handle events
+  while !istaskdone(consumer)
+    handleEvents(protocol)
+    protocol.cancelled && throw(CancelException())
+    sleep(0.05)
   end
 
   # Check tasks
@@ -90,17 +93,13 @@ function measurement(protocol::MPIMeasurementProtocol)
     stack = Base.catch_stack(producer)[1]
     @error stack[1]
     @error stacktrace(stack[2])
-    result = nothing
-  elseif  Base.istaskfailed(consumer)
+  end
+  if  Base.istaskfailed(consumer)
     @error "Consumer failed"
     stack = Base.catch_stack(consumer)[1]
     @error stack[1]
     @error stacktrace(stack[2])
-    result = nothing
-  else
-    result = measState.buffer
   end
-  return result
 end
 
 
@@ -109,30 +108,60 @@ function cleanup(protocol::MPIMeasurementProtocol)
 end
 
 function stop(protocol::MPIMeasurementProtocol)
-  # NOP
+  put!(protocol.biChannel, OperationNotSupportedEvent(StopEvent()))
 end
 
 function resume(protocol::MPIMeasurementProtocol)
-  # NOP
+   put!(protocol.biChannel, OperationNotSupportedEvent(ResumeEvent()))
 end
 
 function cancel(protocol::MPIMeasurementProtocol)
   protocol.cancelled = true
+  put!(protocol.biChannel, OperationNotSupportedEvent(CancelEvent()))
+  # TODO stopTx and reconnect for pipeline and so on
+end
+
+function handleEvent(protocol::MPIMeasurementProtocol, event::RestartEvent)
+  protocol.restart = true
 end
 
 function handleEvent(protocol::MPIMeasurementProtocol, event::DataQueryEvent)
   data = nothing
-  if protocol.done
+  if event.message == "CURRFRAME"
+    data = max(protocol.scanner.seqMeasState.nextFrame - 1, 0)
+  elseif startswith(event.message, "FRAME")
+    frame = tryparse(Int64, split(event.message, ":")[2])
+    if !isnothing(frame) && frame > 0 && frame <= protocol.scanner.seqMeasState.numFrames
+        data = protocol.scanner.seqMeasState.buffer[:, :, :, frame:frame]
+    end
+  elseif event.message == "BUFFER"
     data = protocol.scanner.seqMeasState.buffer
+  else
+    put!(protocol.biChannel, UnknownDataQueryEvent(event))
+    return
   end
   put!(protocol.biChannel, DataAnswerEvent(data, event))
 end
 
 
 function handleEvent(protocol::MPIMeasurementProtocol, event::ProgressQueryEvent)
-  done = protocol.done ? 1 : 0
-  put!(protocol.biChannel, ProgressEvent(done, 1, "Measurement", event))
+  framesTotal = protocol.scanner.seqMeasState.numFrames
+  framesDone = min(protocol.scanner.seqMeasState.nextFrame - 1, framesTotal)
+  put!(protocol.biChannel, ProgressEvent(framesDone, framesTotal, "Frames", event))
 end
 
 handleEvent(protocol::MPIMeasurementProtocol, event::FinishedAckEvent) = protocol.finishAcknowledged = true
 
+function handleEvent(protocol::MPIMeasurementProtocol, event::DatasetStoreStorageRequestEvent)
+  store = event.datastore
+  scanner = protocol.scanner
+  params = event.params
+  data = protocol.scanner.seqMeasState.buffer
+  bgdata = nothing
+  if length(protocol.bgMeas) > 0
+    bgdata = protocol.bgMeas
+  end
+  filename = saveasMDF(store, scanner, protocol.params.sequence, data, params, bgdata = bgdata)
+  @show filename
+  put!(protocol.biChannel, StorageSuccessEvent())
+end
