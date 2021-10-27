e19887c42515d19ae16ce5a6f372d5cfbb329bb3
nHackel <nHackel@users.noreply.github.com>
2021-10-27 11:26:56 +0200

    Added convenince method to convert String to enum if enum name equals string ignoring case. Only applies for listed enums

--- src/Devices/Devices.jl
+++ src/Devices/Devices.jl
@@ -6,3 +6,20 @@ include("Robots/Robots.jl")
 include("SurveillanceUnit/SurveillanceUnit.jl")
 include("TemperatureSensor/TemperatureSensor.jl")
 include("Virtual/Virtual.jl")
+
+
+# List our own enums to avoid accidentally converting a different enum
+# Did not list enums like LakeShoreF71GaussMeterConnectionModes atm, because their convert function uses specific strings
+# and not the enum name
+for enum in [:RPTriggerMode]
+  @eval begin
+    T = $enum
+    function Base.convert(::Type{T}, x::String)
+      try 
+        return stringToEnum(x, T)
+      catch ex
+        throw(ScannerConfigurationError(ex.msg))
+      end
+    end
+  end
+end
