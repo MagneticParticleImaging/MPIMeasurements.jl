fa2aa27ab5593126d56de201ffe0536da676edc7
Tobias Knopp <tobias.knopp@tuhh.de>
2022-04-12 17:06:41 +0200

    add getChannelGroups and enhance dummy temperature sensor

--- src/Devices/TemperatureSensor/TemperatureSensor.jl
+++ src/Devices/TemperatureSensor/TemperatureSensor.jl
@@ -1,4 +1,5 @@
-export TemperatureSensor, getTemperatureSensors, getTemperatureSensor, numChannels, getTemperatures, getTemperature, getChannelNames
+export TemperatureSensor, getTemperatureSensors, getTemperatureSensor, numChannels, getTemperatures, 
+       getTemperature, getChannelNames, getChannelGroups
 
 abstract type TemperatureSensor <: Device end
 
@@ -11,6 +12,9 @@ Base.close(t::TemperatureSensor) = nothing
 @mustimplement numChannels(sensor::TemperatureSensor)
 @mustimplement getTemperatures(sensor::TemperatureSensor)
 @mustimplement getTemperature(sensor::TemperatureSensor, channel::Int)
+@mustimplement getChannelNames(sensor::TemperatureSensor)
+@mustimplement getChannelGroups(sensor::TemperatureSensor)
+
 
 getTemperatureSensors(scanner::MPIScanner) = getDevices(scanner, TemperatureSensor)
 getTemperatureSensor(scanner::MPIScanner) = getDevice(scanner, TemperatureSensor)
