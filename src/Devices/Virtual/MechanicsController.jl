32abac9fa310f72657b29ad5d190c3c75531a5c5
Jonas Schumacher <github735@jonasschumacher.de>
2022-04-27 22:32:08 +0200

    Add cooling controller skeleton

--- src/Devices/Virtual/MechanicsController.jl
+++ src/Devices/Virtual/MechanicsController.jl
@@ -6,16 +6,7 @@ end
 MechanicsControllerParams(dict::Dict) = params_from_dict(MechanicsControllerParams, dict)
 
 Base.@kwdef mutable struct MechanicsController <: VirtualDevice
-  "Unique device ID for this device as defined in the configuration."
-  deviceID::String
-  "Parameter struct for this devices read from the configuration."
-  params::MechanicsControllerParams
-  "Flag if the device is optional."
-	optional::Bool = false
-  "Flag if the device is present."
-  present::Bool = false
-  "Vector of dependencies for this device."
-  dependencies::Dict{String, Union{Device, Missing}}
+  @add_device_fields MechanicsControllerParams
 
   sequence::Union{Sequence, Nothing} = nothing
   channelSteps::Union{Dict{String, Int64}, Nothing} = nothing
--- src/Devices/Virtual/TxDAQController.jl
+++ src/Devices/Virtual/TxDAQController.jl
@@ -15,16 +15,7 @@ struct ControlledChannel
 end
 
 Base.@kwdef mutable struct TxDAQController <: VirtualDevice
-  "Unique device ID for this device as defined in the configuration."
-  deviceID::String
-  "Parameter struct for this devices read from the configuration."
-  params::TxDAQControllerParams
-  "Flag if the device is optional."
-	optional::Bool = false
-  "Flag if the device is present."
-  present::Bool = false
-  "Vector of dependencies for this device."
-  dependencies::Dict{String, Union{Device, Missing}}
+  @add_device_fields TxDAQControllerParams
 
   currTx::Union{Matrix{ComplexF64}, Nothing} = nothing
   desiredTx::Union{Matrix{ComplexF64}, Nothing} = nothing
