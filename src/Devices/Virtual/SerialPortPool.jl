e5b95ca94b22204e274b3246ac4023fd77ee4488
nHackel <nHackel@users.noreply.github.com>
2022-07-13 16:02:02 +0200

    Add description based serialport retrieval

--- src/Devices/Virtual/SerialPortPool.jl
+++ src/Devices/Virtual/SerialPortPool.jl
@@ -48,6 +48,37 @@ function getSerialPort(pool::SerialPortPool, query::String, reply::String, baudr
   return result
 end
 
+function getSerialPort(pool::SerialPortPool, description::String,  baudrate::Integer; ndatabits::Integer = 8, parity::SPParity = SP_PARITY_NONE, nstopbits::Integer = 1)
+  portMap = descriptionMap()
+  if haskey(portMap, description)
+    port = portMap[description]
+    if in(port, pool.activePool)
+      try
+        sp = SerialPort(port)
+        open(sp)
+        set_speed(sp, baudrate)
+        set_frame(sp, ndatabits = ndatabits, parity = parity, nstopbits=nstopbits)
+        reserveSerialPort(pool, port)
+        return sp
+      catch e
+        @error e
+      end
+    end
+  end
+  return nothing
+end
+
+function descriptionMap()
+  result = Dict{String, String}()
+  ports = sp_list_ports()
+  for port in unsafe_wrap(Array, ports, 64, own=false)
+    port == C_NULL && return result
+    result[sp_get_port_description(port)] = sp_get_port_name(port)
+  end
+  sp_free_port_list(ports)
+  return result
+end
+
 function reserveSerialPort(pool::SerialPortPool, port::String)
   deleteat!(pool.activePool, findall(x->x==port, pool.activePool))
 end
