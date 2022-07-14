a1bea84fd66ed76d1ed599d1261b85d69ef508c1
nHackel <nHackel@users.noreply.github.com>
2022-07-14 08:28:02 +0200

    Export descriptionparams for ard.gauss

--- src/Devices/GaussMeter/ArduinoGaussMeter.jl
+++ src/Devices/GaussMeter/ArduinoGaussMeter.jl
@@ -1,4 +1,4 @@
-export ArduinoGaussMeter, ArduinoGaussMeterParams, ArduinoGaussMeterDirectParams, ArduinoGaussMeterPoolParams
+export ArduinoGaussMeter, ArduinoGaussMeterParams, ArduinoGaussMeterDirectParams, ArduinoGaussMeterPoolParams, ArduinoGaussMeterDescriptionParams
 abstract type ArduinoGaussMeterParams <: DeviceParams end
 
 Base.@kwdef struct ArduinoGaussMeterDirectParams <: ArduinoGaussMeterParams
@@ -35,6 +35,24 @@ Base.@kwdef struct ArduinoGaussMeterPoolParams <: ArduinoGaussMeterParams
 end
 ArduinoGaussMeterPoolParams(dict::Dict) = params_from_dict(ArduinoGaussMeterPoolParams, dict)
 
+Base.@kwdef struct ArduinoGaussMeterDescriptionParams <: ArduinoGaussMeterParams
+  description::String
+  coordinateTransformation::Matrix{Float64} = Matrix{Float64}(I,(3,3))
+  calibration::Vector{Float64} = [0.098, 0.098, 0.098]
+
+  commandStart::String = "!"
+  commandEnd::String = "*"
+  pause_ms::Int = 30
+  timeout_ms::Int = 1000
+  delim::String = "#"
+  baudrate::Integer = 115200
+  ndatabits::Integer = 8
+  parity::SPParity = SP_PARITY_NONE
+  nstopbits::Integer = 1
+end
+ArduinoGaussMeterDescriptionParams(dict::Dict) = params_from_dict(ArduinoGaussMeterDescriptionParams, dict)
+
+
 Base.@kwdef mutable struct ArduinoGaussMeter <: GaussMeter
   @add_device_fields ArduinoGaussMeterParams
   ard::Union{SimpleArduino, Nothing} = nothing
@@ -46,7 +64,7 @@ optionalDependencies(::ArduinoGaussMeter) = [SerialPortPool]
 function _init(gauss::ArduinoGaussMeter)
   params = gauss.params
   sp = initSerialPort(gauss, params)
-  sd = SerialDevice(sp, params.pause_ms, params.timeout_ms, params.delim, params.delim)
+  sd = SerialDevice(sp, params.pause_ms, params.timeout_ms, "", "")
   ard = SimpleArduino(;commandStart = params.commandStart, commandEnd = params.commandEnd, delim = params.delim, sd = sd)
   gauss.ard = ard
 end
@@ -74,6 +92,20 @@ function initSerialPort(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterPoolP
   if hasDependency(gauss, SerialPortPool)
     pool = dependency(gauss, SerialPortPool)
     sp = getSerialPort(pool, "!VERSION*#", "HALLSENS:1:$(params.position)#", params.baudrate, ndatabits=params.ndatabits,parity=params.parity,nstopbits=params.nstopbits)
+    if isnothing(sp)
+      throw(ScannerConfigurationError("Device $(deviceID(gauss)) found no fitting serial port."))
+    end
+    return sp
+  else
+    throw(ScannerConfigurationError("Device $(deviceID(gauss)) requires a SerialPortPool dependency but has none."))
+  end
+end
+
+function initSerialPort(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterDescriptionParams)
+  pool = nothing
+  if hasDependency(gauss, SerialPortPool)
+    pool = dependency(gauss, SerialPortPool)
+    sp = getSerialPort(pool, params.description, params.baudrate, ndatabits=params.ndatabits,parity=params.parity,nstopbits=params.nstopbits)
     if isnothing(sp)
       throw(ScannerConfigurationError("Device $(deviceID(gauss)) found no fitting serial port."))
     end
