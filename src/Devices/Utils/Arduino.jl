682a8cd0edb63f5f679c7a33af654e9cb9d5bde5
nHackel <nHackel@users.noreply.github.com>
2022-07-18 17:51:06 +0200

    Update SerialDevice to use new LibSerialport functions

--- src/Devices/Utils/Arduino.jl
+++ src/Devices/Utils/Arduino.jl
@@ -2,29 +2,34 @@ export Arduino, sendCommand
 
 abstract type Arduino <: Device end
 
+
 @mustimplement cmdStart(ard::Arduino)
 @mustimplement cmdEnd(ard::Arduino)
-@mustimplement cmdDelim(ard::Arduino)
 @mustimplement serialDevice(ard::Arduino)
 
 function sendCommand(ard::Arduino, cmdString::String)
-  cmd = cmdStart(ard) * cmdString * cmdEnd(ard) * cmdDelim(ard)
+  cmd = cmdStart(ard) * cmdString * cmdEnd(ard)
   return query(serialDevice(ard), cmd)
 end
 
 function sendCommand(ard::Arduino, cmdString::String, data::AbstractArray)
-  cmd = cmdStart(ard) * cmdString * cmdEnd(ard) * cmdDelim(ard)
-  return query!(serialDevice(ard), cmd, data)
+  cmd = cmdStart(ard) * cmdString * cmdEnd(ard)
+  return query!(serialDevice(ard), cmd, data, delimited = true)
 end
 
 Base.@kwdef struct SimpleArduino <: Arduino
   commandStart::String = "!"
   commandEnd::String = "*"
-  delim::String = "#"
   sd::SerialDevice
 end
 cmdStart(ard::SimpleArduino) = ard.commandStart
 cmdEnd(ard::SimpleArduino) = ard.commandEnd
-cmdDelim(ard::SimpleArduino) = ard.delim
 serialDevice(ard::SimpleArduino) = ard.sd
 close(ard::SimpleArduino) = close(ard.sd)
+
+macro add_arduino_fields(cmdStart, cmdEnd)
+  return esc(quote
+    commandStart::String = $cmdStart
+    commandEnd::String = $cmdEnd
+  end)
+end
