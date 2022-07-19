fe6fc22215e237f66387614436c8c788da69c7f9
nHackel <nHackel@users.noreply.github.com>
2022-07-19 11:59:41 +0200

    Update ArduinoGaussMeter

--- src/Devices/GaussMeter/ArduinoGaussMeter.jl
+++ src/Devices/GaussMeter/ArduinoGaussMeter.jl
@@ -6,15 +6,8 @@ Base.@kwdef struct ArduinoGaussMeterDirectParams <: ArduinoGaussMeterParams
   coordinateTransformation::Matrix{Float64} = Matrix{Float64}(I,(3,3))
   calibration::Vector{Float64} = [0.098, 0.098, 0.098]
 
-  commandStart::String = "!"
-  commandEnd::String = "*"
-  pause_ms::Int = 30
-  timeout_ms::Int = 1000
-  delim::String = "#"
-  baudrate::Integer = 9600
-  ndatabits::Integer = 8
-  parity::SPParity = SP_PARITY_NONE
-  nstopbits::Integer = 1
+  @add_serial_device_fields '#'
+  @add_arduino_fields "!" "*"
 end
 ArduinoGaussMeterDirectParams(dict::Dict) = params_from_dict(ArduinoGaussMeterDirectParams, dict)
 
@@ -23,15 +16,8 @@ Base.@kwdef struct ArduinoGaussMeterPoolParams <: ArduinoGaussMeterParams
   coordinateTransformation::Matrix{Float64} = Matrix{Float64}(I,(3,3))
   calibration::Vector{Float64} = [0.098, 0.098, 0.098]
 
-  commandStart::String = "!"
-  commandEnd::String = "*"
-  pause_ms::Int = 30
-  timeout_ms::Int = 1000
-  delim::String = "#"
-  baudrate::Integer = 115200
-  ndatabits::Integer = 8
-  parity::SPParity = SP_PARITY_NONE
-  nstopbits::Integer = 1
+  @add_serial_device_fields '#'
+  @add_arduino_fields "!" "*"
 end
 ArduinoGaussMeterPoolParams(dict::Dict) = params_from_dict(ArduinoGaussMeterPoolParams, dict)
 
@@ -40,15 +26,8 @@ Base.@kwdef struct ArduinoGaussMeterDescriptionParams <: ArduinoGaussMeterParams
   coordinateTransformation::Matrix{Float64} = Matrix{Float64}(I,(3,3))
   calibration::Vector{Float64} = [0.098, 0.098, 0.098]
 
-  commandStart::String = "!"
-  commandEnd::String = "*"
-  pause_ms::Int = 30
-  timeout_ms::Int = 1000
-  delim::String = "#"
-  baudrate::Integer = 115200
-  ndatabits::Integer = 8
-  parity::SPParity = SP_PARITY_NONE
-  nstopbits::Integer = 1
+  @add_serial_device_fields '#'
+  @add_arduino_fields "!" "*"
 end
 ArduinoGaussMeterDescriptionParams(dict::Dict) = params_from_dict(ArduinoGaussMeterDescriptionParams, dict)
 
@@ -63,60 +42,54 @@ optionalDependencies(::ArduinoGaussMeter) = [SerialPortPool]
 
 function _init(gauss::ArduinoGaussMeter)
   params = gauss.params
-  sp = initSerialPort(gauss, params)
-  sd = SerialDevice(sp, params.pause_ms, params.timeout_ms, "", "")
-  ard = SimpleArduino(;commandStart = params.commandStart, commandEnd = params.commandEnd, delim = params.delim, sd = sd)
+  sd = initSerialDevice(gauss, params)
+  ard = SimpleArduino(;commandStart = params.commandStart, commandEnd = params.commandEnd, sd = sd)
   gauss.ard = ard
 end
 
-function initSerialPort(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterDirectParams)
-  spTU = SerialPort(params.portAddress)
-  open(spTU)
-  set_speed(spTU, params.baudrate)
-  set_frame(spTU,ndatabits=params.ndatabits,parity=params.parity,nstopbits=params.nstopbits)
-  flush(spTU)
-  write(spTU, "!VERSION*#")
-  response=readuntil(spTU, Vector{Char}(params.delim), params.timeout_ms);
-  @info response
+function initSerialDevice(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterDirectParams)
+  sd = SerialDevice(params.portAddress; serial_device_splatting(params)...)
+  response = query(sd, "!VERSION*")
+  @debug response
   if(!(startswith(response, "HALLSENS:1")) ) 
-      close(spTU)
+      close(sd)
       throw(ScannerConfigurationError(string("Connected to wrong Device", response)))
     else
       @info "Connection to ArduinoTempBox established."        
   end
-  return spTU
+  return sd
 end
 
-function initSerialPort(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterPoolParams)
+function initSerialDevice(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterPoolParams)
   pool = nothing
   if hasDependency(gauss, SerialPortPool)
     pool = dependency(gauss, SerialPortPool)
-    sp = getSerialPort(pool, "!VERSION*#", "HALLSENS:1:$(params.position)#", params.baudrate, ndatabits=params.ndatabits,parity=params.parity,nstopbits=params.nstopbits)
-    if isnothing(sp)
+    sd = getSerialDevice(pool, "!VERSION*", "HALLSENS:1:$(params.position)"; serial_device_splatting(params)...)
+    if isnothing(sd)
       throw(ScannerConfigurationError("Device $(deviceID(gauss)) found no fitting serial port."))
     end
-    return sp
+    return sd
   else
     throw(ScannerConfigurationError("Device $(deviceID(gauss)) requires a SerialPortPool dependency but has none."))
   end
 end
 
-function initSerialPort(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterDescriptionParams)
+function initSerialDevice(gauss::ArduinoGaussMeter, params::ArduinoGaussMeterDescriptionParams)
   pool = nothing
   if hasDependency(gauss, SerialPortPool)
     pool = dependency(gauss, SerialPortPool)
-    sp = getSerialPort(pool, params.description, params.baudrate, ndatabits=params.ndatabits,parity=params.parity,nstopbits=params.nstopbits)
-    if isnothing(sp)
+    sd = getSerialDevice(pool, params.description; serial_device_splatting(params)...)
+    if isnothing(sd)
       throw(ScannerConfigurationError("Device $(deviceID(gauss)) found no fitting serial port."))
     end
-    return sp
+    return sd
   else
     throw(ScannerConfigurationError("Device $(deviceID(gauss)) requires a SerialPortPool dependency but has none."))
   end
 end
 
 function getXYZValues(gauss::ArduinoGaussMeter)
-  data = Array{Int16}(undef, 3)
+  data = zeros(Int16, 3)
   sendCommand(gauss.ard, "DATA", data)
   #TODO
   return data
