c1f9c14fc756c85b5c4d7d299073e4e91932a86e
nHackel <nHackel@users.noreply.github.com>
2022-08-02 10:55:17 +0200

    Init serialisation to toml

---
+++ src/Utils/StructToToml.jl
@@ -0,0 +1,28 @@
+export toTOML, toDict, toDict!, toDictValue
+
+function toTOML(fileName::AbstractString, value)
+  open(fileName, "w") do io
+    toTOML(io, value)
+  end
+end
+
+function toTOML(io::IO, value)
+  dict = toDict(value)
+  TOML.print(io, dict)
+end
+
+function toDict(value)
+  dict = Dict{String, Any}()
+  return toDict!(dict, value)
+end
+
+function toDict!(dict::Dict{String, Any}, value)
+  for field in fieldnames(typeof(value))
+    dict[String(field)] = toDictValue(getproperty(value, field))
+  end
+  return dict
+end
+
+toDictValue(x) = x
+toDictValue(x::T) where {T<:Quantity} = strip(string(x))
+toDictValue(x::Array) = toDictValue.(x)
