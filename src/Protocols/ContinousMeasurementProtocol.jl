8e29fa2d360a8e072cd9f5067e19fc4671ae62af
nHackel <nHackel@users.noreply.github.com>
2022-09-29 18:19:18 +0200

    Add Bg meas to cont measurement protocol + queries

--- src/Protocols/ContinousMeasurementProtocol.jl
+++ src/Protocols/ContinousMeasurementProtocol.jl
@@ -3,10 +3,16 @@ export ContinousMeasurementProtocol, ContinousMeasurementProtocolParams
 Parameters for the MPIMeasurementProtocol
 """
 Base.@kwdef mutable struct ContinousMeasurementProtocolParams <: ProtocolParams
+  "Foreground frames to measure. Overwrites sequence frames"
+  fgFrames::Int64 = 1
+  "Background frames to measure. Overwrites sequence frames"
+  bgFrames::Int64 = 1
   "Pause between measurements"
   pause::typeof(1.0u"s") = 0.2u"s"
   "If set the tx amplitude and phase will be set with control steps"
   controlTx::Bool = false
+  "If unset no background measurement will be taken"
+  measureBackground::Bool = true
   "Sequence to measure"
   sequence::Union{Sequence, Nothing} = nothing
 end
@@ -30,11 +36,13 @@ Base.@kwdef mutable struct ContinousMeasurementProtocol <: Protocol
 
 
   latestMeas::Array{Float32, 4} = zeros(Float32, 0, 0, 0, 0)
+  latestBgMeas::Array{Float32, 4} = zeros(Float32, 0, 0, 0, 0)
   stopped::Bool = false
   cancelled::Bool = false
   finishAcknowledged::Bool = false
   measuring::Bool = false
   txCont::Union{TxDAQController, Nothing} = nothing
+  unit::String = ""
   counter::Int64 = 0
 end
 
@@ -83,10 +91,24 @@ end
 function _execute(protocol::ContinousMeasurementProtocol)
   @info "Measurement protocol started"
 
+  if protocol.params.measureBackground
+    if askChoices(protocol, "Press continue when background measurement can be taken", ["Cancel", "Continue"]) == 1
+      throw(CancelException())
+    end
+    acqNumFrames(protocol.params.sequence, protocol.params.bgFrames)
 
+    @debug "Taking background measurement."
+    protocol.unit = "BG Measurement"
+    protocol.latestBgMeas = measurement(protocol)
+    if askChoices(protocol, "Press continue when foreground loop can be started", ["Cancel", "Continue"]) == 1
+      throw(CancelException())
+    end
+  end
+
+  protocol.unit = "Measurements"
   while true
     @info "Taking new measurement"
-    measurement(protocol)
+    protocol.latestMeas = measurement(protocol)
     protocol.counter+=1
 
     measPauseOver = false
@@ -158,7 +180,7 @@ function measurement(protocol::ContinousMeasurementProtocol)
     throw(ErrorException("Measurement failed, see logged exceptions and stacktraces"))
   end
 
-  protocol.latestMeas = copy(measState.buffer)
+  return copy(measState.buffer)
 end
 
 function asyncMeasurement(protocol::ContinousMeasurementProtocol)
@@ -193,13 +215,29 @@ function cancel(protocol::ContinousMeasurementProtocol)
 end
 
 function handleEvent(protocol::ContinousMeasurementProtocol, event::DataQueryEvent)
-  data = protocol.latestMeas
+  data = nothing
+  if event.message == "FG"
+    if length(protocol.latestMeas) > 0
+      data = copy(protocol.latestMeas)
+    else
+      data = nothing
+    end
+  elseif event.message == "BG"
+    if length(protocol.latestBgMeas) > 0
+      data = copy(protocol.latestBgMeas)
+    else
+      data = nothing
+    end
+  else
+    put!(protocol.biChannel, UnknownDataQueryEvent(event))
+    return
+  end
   put!(protocol.biChannel, DataAnswerEvent(data, event))
 end
 
 
 function handleEvent(protocol::ContinousMeasurementProtocol, event::ProgressQueryEvent)
-  reply = ProgressEvent(protocol.counter, 0, "Measurements", event)  
+  reply = ProgressEvent(protocol.counter, 0, protocol.unit, event)  
   put!(protocol.biChannel, reply)
 end
 
