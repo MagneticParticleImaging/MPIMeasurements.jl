d7ec15b7f01065b6842d39928cfb66ea3973c0b0
nHackel <nHackel@users.noreply.github.com>
2022-11-30 09:30:07 +0100

    Added TDesignCube skeleton

---
+++ src/Devices/GaussMeter/TDesignCube.jl
@@ -0,0 +1,40 @@
+export 
+
+Base.@kwdef struct TDesignCubeParams <: DeviceParams
+    T::Int64
+    N::Int64
+    radius::typeof(1.0u"mm") = 0.0u"mm"
+    samplesSize::Union{Nothing, Int64} = nothing # Optional overwrite
+end
+
+Base.@kwdef mutable struct TDesignCube
+    @add_device_fields TDesignCubeParams
+    sensors::Union{Vector{ArduinoGaussMeter}, Nothing} = nothing
+end
+
+neededDependencies(::ArduinoGaussMeter) = [ArduinoGaussMeter]
+optionalDependencies(::ArduinoGaussMeter) = []
+
+function _init(cube::TDesignCube)
+    sensors = dependencies(cube, ArduinoGaussMeter)
+    # TODO Check if all needed sensors are there
+    # TODO Sort sensors according to position
+    cube.sensors = sensors
+    if !isnothing(cube.params.sampleSize)
+        # TODO overwrite samplesize of sensors
+    end
+end
+
+# TODO get/setSampleSize
+
+function getXYZValues(cube::TDesignCube)
+    measurement = zeros(typeof(u"T"), 3, cube.params.T)
+    # TODO Implement this with start/receive from sensors
+    return measurement
+end
+
+# TODO implement start and receive like with sensor as (maybe async) for loops
+
+function close(cube::TDesignCube)
+    # NOP
+end
