0b8dbf8d1f1b56fd830d5db00df3a20601ad0296
vincent <vincent.ridder@tuhh.de>
2023-01-30 14:57:19 +0100

    Updated SEnsor software to 3.0 removing position fixing varianz to n-1 adding comments

--- Devices/Arduino/HallSensor/HallSensor.ino
+++ Devices/Arduino/HallSensor/HallSensor.ino
@@ -1,5 +1,5 @@
 #define ARDUINO_TYPE "HALLSENS"
-#define VERSION "2.1"
+#define VERSION "3.0"
 #define BAUDRATE 9600
 #define MEASDELAY 2
 
@@ -7,39 +7,40 @@
 
 // Tlv493d Opject
 Tle493d_w2b6 sensor = Tle493d_w2b6(Tle493d::MASTERCONTROLLEDMODE);
-int sample_size= 1000;
+int sample_size = 1000;
 
 // Communication
 #define INPUT_BUFFER_SIZE 3000
 #define VALUE_BUFFER_SIZE 1024
 char input_buffer[INPUT_BUFFER_SIZE];
-int16_t value_buffer[VALUE_BUFFER_SIZE*3];
+int16_t value_buffer[VALUE_BUFFER_SIZE * 3];
 unsigned int input_pos = 0;
 
-int getData(char*);
-int getPosition(char*);
-int getTemp(char*);
-int getVersion(char*);
-int getCommands(char*);
-int setSampleSize(char*);
-//int setFoo(char*)
+int getDelay(char *);
+int getData(char *);
+int getPosition(char *);
+int getTemp(char *);
+int getVersion(char *);
+int getCommands(char *);
+int setSampleSize(char *);
 
-typedef struct {
-  const char * id;
-  int (*handler)(char*);
+typedef struct
+{
+  const char *id;
+  int (*handler)(char *);
 } commandHandler_t;
 
 commandHandler_t cmdHandler[] = {
-  {"DELAY",getDelay}
-  {"DATA", getData},
-  {"TEMP", getTemp},
-  {"VERSION", getVersion},
-  {"COMMANDS", getCommands},
-  {"SAMPLES", setSampleSize}
-  //{"FOO", setFoo}
+    {"DELAY", getDelay},
+    {"DATA", getData},
+    {"TEMP", getTemp},
+    {"VERSION", getVersion},
+    {"COMMANDS", getCommands},
+    {"SAMPLES", setSampleSize}
 };
 
-int getCommands(char*) {
+int getCommands(char *)
+{
   Serial.print("Valid Commands (without quotes):");
   Serial.print("'!DATA*#' ");
   Serial.print("'!DELAY*#' ");
@@ -50,60 +51,72 @@ int getCommands(char*) {
   Serial.println("#");
 }
 
-
-bool updateBufferUntilDelim(char delim) {
+bool updateBufferUntilDelim(char delim)
+{
   char nextChar;
-  while (Serial.available() > 0) {
-      nextChar = Serial.read();
-      if (nextChar == delim) {
-        input_buffer[input_pos] = '\0';
-        input_pos = 0;
-        return true;
-      } else if (nextChar != '\n') {
-        input_buffer[input_pos % (INPUT_BUFFER_SIZE - 1)] = nextChar; // Size - 1 to always leave room for \0
-        input_pos++;
-      }
+  while (Serial.available() > 0)
+  {
+    nextChar = Serial.read();
+    if (nextChar == delim)
+    {
+      input_buffer[input_pos] = '\0';
+      input_pos = 0;
+      return true;
+    }
+    else if (nextChar != '\n')
+    {
+      input_buffer[input_pos % (INPUT_BUFFER_SIZE - 1)] = nextChar; // Size - 1 to always leave room for \0
+      input_pos++;
+    }
   }
   return false;
 }
 
-void serialCommand() {
-  int s=-1;
-  int e=-1;
-  char beginDelim='!';
+void serialCommand()
+{
+  int s = -1;
+  int e = -1;
+  char beginDelim = '!';
   char *beginCmd;
-  char endDelim='*';
+  char endDelim = '*';
   char *endCmd;
   char *command;
   bool done = false;
   bool unknown = false;
-  
+
   // determin substring
-  if(Serial.available()>0) {
-    done = updateBufferUntilDelim('#');   
+  if (Serial.available() > 0)
+  {
+    done = updateBufferUntilDelim('#');
   }
 
-  if (done) {
+  if (done)
+  {
     s = -1;
-    if ((beginCmd = strchr(input_buffer, beginDelim)) != NULL) {
+    if ((beginCmd = strchr(input_buffer, beginDelim)) != NULL)
+    {
       s = beginCmd - input_buffer;
     }
 
     e = -1;
-    if ((endCmd = strchr(input_buffer, endDelim)) != NULL) {
+    if ((endCmd = strchr(input_buffer, endDelim)) != NULL)
+    {
       e = endCmd - input_buffer;
-    }   
+    }
 
     unknown = true;
-    //check if valid command
-    if (e!=-1 && s!=-1) {
+    // check if valid command
+    if (e != -1 && s != -1)
+    {
       command = beginCmd + 1;
       *endCmd = '\0';
       Serial.flush();
 
-      //check for known commands
-      for (int i = 0; i < sizeof(cmdHandler)/sizeof(*cmdHandler); i++) {
-        if (strncmp(cmdHandler[i].id, command, strlen(cmdHandler[i].id)) == 0) {
+      // check for known commands
+      for (int i = 0; i < sizeof(cmdHandler) / sizeof(*cmdHandler); i++)
+      {
+        if (strncmp(cmdHandler[i].id, command, strlen(cmdHandler[i].id)) == 0)
+        {
           cmdHandler[i].handler(command);
           unknown = false;
           input_buffer[0] = '\0'; // "Empty" input buffer
@@ -111,133 +124,150 @@ void serialCommand() {
         }
       }
     }
-    
-    if (unknown) {
+
+    if (unknown)
+    {
       Serial.println("UNKNOWN");
       Serial.println("#");
-      Serial.flush();  
+      Serial.flush();
     }
-
   }
-
 }
 ////////////////////////////////////////////////////////////////////////////////
 
-void setup() {
+void setup()
+{
   Serial.begin(BAUDRATE);
-  while(!Serial);
- 
-  //If using the MS2Go-Kit: Enable following lines to switch on the sensor
-  // ***
+  while (!Serial)
+    ;
+
+  // If using the MS2Go-Kit: Enable following lines to switch on the sensor
+  //  ***
   pinMode(LED2, OUTPUT);
   digitalWrite(LED2, HIGH);
   delay(50);
   // ***
-  
+
   sensor.begin();
   sensor.disableTemp();
 }
-int getDelay(char*) {
-  
-}
-
-int getData(char*) {
-  // updateData reads values from sensor and reading triggers next measurement
-  sensor.updateData(); 
-  delay(MEASDELAY); 
-  unsigned long start, end,startFP,endA;
-    // TODO consistent formatting
-  int16_t x=0,y=0,z=0;
-  int32_t sumX=0,sumY=0,sumZ=0,sumXX=0,sumYY=0,sumZZ=0;
-  float varX=0, varY = 0, varZ=0, meanX =0, meanY =0, meanZ =0;
-  for (int i =0 ;i< sample_size*3 ; i+=3){
-    sensor.updateData(); 
-    start = millis();
-    
-    x = sensor.getRawX();
-    y = sensor.getRawY();
-    z = sensor.getRawZ();
-    
-    value_buffer[i] = x;
-    value_buffer[i+1] = y;
-    value_buffer[i+2] = z;
-    
-    sumX+=x;
-    sumY+=y;
-    sumZ+=z;
-    
-    end = millis();
-    if (end - start < measDelay) {
-      delay(MEASDELAY-(end - start));
-    }
-  }
-  
-  meanX = (float)sumX/sample_size;
-  meanY = (float)sumY/sample_size;
-  meanZ = (float)sumZ/sample_size;
-  
-  for(int i=0; i<sample_size*3;i+=3){
-    sumXX += ((float)value_buffer[i]-meanX)*((float)value_buffer[i]-meanX);
-    sumYY += ((float)value_buffer[i+1]-meanY)*((float)value_buffer[i+1]-meanY);
-    sumZZ += ((float)value_buffer[i+2]-meanZ) *((float)value_buffer[i+2]-meanZ);
-  }
-
-  varX =(float)sumXX/sample_size;
-  varY =(float)sumYY/sample_size;
-  varZ =(float)sumZZ/sample_size;
-
-  Serial.print(meanX,7);
-  Serial.print(",");
-  Serial.print(meanY,7);
-  Serial.print(",");
-  Serial.print(meanZ,7);
-  Serial.print(",");
-  Serial.print(varX,7);
-  Serial.print(",");
-  Serial.print(varY,7);
-  Serial.print(",");
-  Serial.print(varZ,7);
+int getDelay(char *)
+{
+  Serial.print(MEASDELAY, 7);
   Serial.println("#");
   Serial.flush();
 }
 
+int getData(char *)
+{
+  unsigned long start, end, startFP, endA;
+  int16_t x = 0, y = 0, z = 0;
+  int32_t sumX = 0, sumY = 0, sumZ = 0;
+  float sumXX = 0, sumYY = 0, sumZZ = 0;
+  float varX = 0, varY = 0, varZ = 0;
+  float meanX = 0, meanY = 0, meanZ = 0;
 
-int getTemp(char*) {
+  // updateData reads values from sensor and reading triggers next measurement
+  sensor.updateData();
+  delay(MEASDELAY);
+
+  // calculating mean values Mean =sum(x[i])/sample_size
+  // measurement is stored for calculating the variance in a second go
+  for (int i = 0; i < sample_size * 3; i += 3)
+  {
+    sensor.updateData();
+    start = millis();
+
+    x = sensor.getRawX();
+    y = sensor.getRawY();
+    z = sensor.getRawZ();
+
+    value_buffer[i] = x;
+    value_buffer[i + 1] = y;
+    value_buffer[i + 2] = z;
+
+    sumX += x;
+    sumY += y;
+    sumZ += z;
+
+    //waiting until new data is ready in the sensor
+    end = millis();
+    if (end - start < MEASDELAY)
+    {
+      delay(MEASDELAY - (end - start));
+    }
+  }
+  meanX = (float)sumX / sample_size;
+  meanY = (float)sumY / sample_size;
+  meanZ = (float)sumZ / sample_size;
+
+  // calculating var var(x) = sum((x[i]-x_mean)^2)/(sample_size-1)
+  // for sample_size = 1 the variance is zero
+  if (sample_size > 1)
+  {
+    for (int i = 0; i < sample_size * 3; i += 3)
+    {
+      sumXX += ((float)value_buffer[i] - meanX) * ((float)value_buffer[i] - meanX);
+      sumYY += ((float)value_buffer[i + 1] - meanY) * ((float)value_buffer[i + 1] - meanY);
+      sumZZ += ((float)value_buffer[i + 2] - meanZ) * ((float)value_buffer[i + 2] - meanZ);
+    }
+
+    varX = sumXX / (sample_size - 1);
+    varY = sumYY / (sample_size - 1);
+    varZ = sumZZ / (sample_size - 1);
+  }
+  Serial.print(meanX, 7);
+  Serial.print(",");
+  Serial.print(meanY, 7);
+  Serial.print(",");
+  Serial.print(meanZ, 7);
+  Serial.print(",");
+  Serial.print(varX, 7);
+  Serial.print(",");
+  Serial.print(varY, 7);
+  Serial.print(",");
+  Serial.print(varZ, 7);
+  Serial.println("#");
+  Serial.flush();
+}
+
+int getTemp(char *)
+{
   sensor.enableTemp();
-  delay(10); 
+  delay(10);
   sensor.updateData();
   delay(MEASDELAY);
   sensor.updateData();
   delay(MEASDELAY);
   float temp = sensor.getTemp();
-  Serial.print(temp,7);
+  Serial.print(temp, 7);
   Serial.println("#");
   Serial.flush();
   sensor.disableTemp();
   delay(10);
 }
 
-int getVersion(char*) {
+int getVersion(char *)
+{
   Serial.print(ARDUINO_TYPE);
   Serial.print(":");
   Serial.print(VERSION);
-  Serial.print(":");
-  Serial.print(POSITION);
   Serial.print("#");
-  Serial.flush(); 
+  Serial.flush();
 }
 
-int setSampleSize(char* command){
-  int value_int = atoi(command+7); // Could point to end of char, value_int = 0. But in theory can not differentiate between unintended string and 0 samples set
-  if (value_int>0 && value_int<=VALUE_BUFFER_SIZE){
-    sample_size=value_int;
+int setSampleSize(char *command)
+{
+  int value_int = atoi(command + 7); // Could point to end of char, value_int = 0. But in theory can not differentiate between unintended string and 0 samples set
+  if (value_int > 0 && value_int <= VALUE_BUFFER_SIZE)
+  {
+    sample_size = value_int;
   }
   Serial.print(sample_size);
   Serial.println("#");
 }
 
-
-
-void loop() {
+void loop()
+{
   serialCommand();
 }
