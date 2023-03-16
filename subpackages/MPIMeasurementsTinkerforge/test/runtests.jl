2926b7497a5c1ca7e0551088c783435171957ec8
Jonas Schumacher <github735@jonasschumacher.de>
2023-03-16 12:25:58 +0100

    Remove testing of stale deps for Tinkerforge subpackage

--- subpackages/MPIMeasurementsTinkerforge/test/runtests.jl
+++ subpackages/MPIMeasurementsTinkerforge/test/runtests.jl
@@ -5,7 +5,7 @@ using MPIMeasurementsTinkerforge
 
 @testset "MPIMeasurementsTinkerforge" begin
   @testset "Aqua" begin
-    Aqua.test_all(MPIMeasurementsTinkerforge, ambiguities=false)
+    Aqua.test_all(MPIMeasurementsTinkerforge, ambiguities=false, stale_deps=false) # Stale deps deactivated due to errors with python packages
   end
 
   # Add tests here
